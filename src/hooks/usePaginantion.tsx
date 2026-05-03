a5299515c9fd7a095304261e0a6b285eafe6e865
Jasmine <jasmineoliver158@gmail.com>
2026-05-03 15:43:07 -0400

    intialized state for usePagination

--- src/hooks/usePaginantion.tsx
+++ src/hooks/usePaginantion.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
 export function usePagination(totalItems: number, itemsPerPage: number = 10, initialPage: number = 1) {
-    
+    const [currentPage, setCurrentPage] = useState(initialPage);
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+
 }
