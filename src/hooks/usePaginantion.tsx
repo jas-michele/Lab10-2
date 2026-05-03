c1cb5c434577fdd355c2a32da8da0d36ceb00aee
Jasmine <jasmineoliver158@gmail.com>
2026-05-03 16:04:20 -0400

    Implement pagination logic in usePagination hook

--- src/hooks/usePaginantion.tsx
+++ src/hooks/usePaginantion.tsx
@@ -2,6 +2,23 @@ import { useState } from "react";
 
 export function usePagination(totalItems: number, itemsPerPage: number = 10, initialPage: number = 1) {
     const [currentPage, setCurrentPage] = useState(initialPage);
+
     const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+
+    const itemsOnCurrentPage = Math.min( itemsPerPage, totalItems - startIndex);
+
+    const setPage = (page: number) => {
+        if (page < 1 || page > totalPages) return;
+        setCurrentPage(page)
+    }
+
+    const nextPage = () => setPage(currentPage + 1);
+    const prevPage = () => setPage(currentPage - 1);
+
+    const canNextPage = currentPage < totalPages;
+    const canPrevPage = currentPage > 1;
 
 }
